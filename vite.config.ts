dc0fe7a85efa6006c2268c69779bb65d35b03795
Karliky <karliky@gmail.com>
2026-01-13 22:56:35 +0100

    chore: vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,21 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    const isProd = mode === 'production';
-    return {
-      base: isProd ? '/InnioasisY1Themes-tool/' : '/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  const isProd = mode === 'production';
+  return {
+    base: '/',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
